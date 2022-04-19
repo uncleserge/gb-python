52a247159238c42ef06f71f57c7380b13e67a151
UncleSerge <serge@sadowsky.spb.ru>
2022-04-19 15:00:57 +0300

    Lesson 5 task 3 ready

--- Lesson5/task3.py
+++ Lesson5/task3.py
@@ -9,3 +9,19 @@
 # Иванов 23543.12
 # Петров 13749.32
 
+from functools import reduce
+
+with open('task3.txt', 'r', encoding="utf-8") as read_f:
+    lines = read_f.readlines()
+
+count = len(lines)
+xxx = ',\n'.join('{}'.format(surname) for surname, salary in (el.split() for el in lines) if float(salary) > 20000)
+print(xxx)
+
+
+print('Сотрудники с окладом более 20000 у.е.:\n' +
+      ',\n'.join('{}'.format(surname) for surname, salary in (el.split() for el in lines) if float(salary) > 20000) +
+      '\n')
+
+average_income = reduce(lambda a, b: a + b, {float(salary) for surname, salary in (el.split() for el in lines)})/count
+print(f'Среднеяя величина дохода сотрудников: {average_income:.2f} у.е.')
