717c8a94e01ffeed7d6d3e8dc75986d8c844469b
UncleSerge <serge@sadowsky.spb.ru>
2022-04-19 14:14:10 +0300

    Lesson 5 task 1 ready

--- Lesson5/task1.py
+++ Lesson5/task1.py
@@ -3,4 +3,17 @@
 # Создать программный файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных будет свидетельствовать пустая строка.
 
+print("Вводите текст. Для завершения введите пустую строку.")
 
+with open('my_file.txt', 'w') as write_f:
+    while True:
+        line = input(': ')
+        if not line:
+            break
+
+        write_f.write(line + '\n')
+
+print('Результат работы:\n')
+with open('my_file.txt') as read_f:
+    for read_line in read_f:
+        print(read_line, end='')
