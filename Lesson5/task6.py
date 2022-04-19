5ecba489d6a18bc287d5281abdd243aeb350ba4a
UncleSerge <serge@sadowsky.spb.ru>
2022-04-19 17:03:39 +0300

    Lesson 5 task 6 ready

--- Lesson5/task6.py
+++ Lesson5/task6.py
@@ -10,3 +10,24 @@
 # Физкультура: — 30(пр) —
 # Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
+import re
+from functools import reduce
+
+subjects = {}
+try:
+    with open('task6.txt', encoding='utf-8') as read_f:
+        lines = read_f.read().splitlines()
+except IOError:
+    print("Произошла ошибка ввода-вывода!")
+else:
+    for line in lines:
+        subject, hours = line.split(':')
+        # get tuple of hours for current subject
+        hours = (int(el[0]) for el in (re.findall(r'\d+', el) for el in re.split(r'\s+', hours.strip())) if len(el) > 0)
+        # count total hours
+        total_hours = reduce(lambda a, b: a + b, hours, 0)
+        print(subject, total_hours)
+        # put result to dictionary
+        subjects[subject] = total_hours
+
+print(subjects)
