d3010848c5785befd88924a8441f8b1b42cff67b
UncleSerge <serge@sadowsky.spb.ru>
2022-04-19 14:42:35 +0300

    Lesson 5 task 2 ready

--- Lesson5/task2.py
+++ Lesson5/task2.py
@@ -3,3 +3,17 @@
 # Создать текстовый файл (не программно), сохранить в нём несколько строк,
 # выполнить подсчёт строк и слов в каждой строке.
 
+
+def read_line_from_file(file_name):
+    with open(file_name) as read_f:
+        for line in read_f:
+            yield line
+
+
+lines = read_line_from_file("task2.txt")
+str_count = 0
+for el in lines:
+    str_count += 1
+    print(f'В строка {str_count} кол-во слов {len(el.split())}')
+
+print(f'Количество строк: {str_count}')
