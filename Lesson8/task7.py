8bfb8775958fa0d685cc6aa656e60d719a4245a9
UncleSerge <serge@sadowsky.spb.ru>
2022-05-01 09:36:44 +0300

    Lesson 8 task 7 ready

--- Lesson8/task7.py
+++ Lesson8/task7.py
@@ -7,12 +7,46 @@
 
 
 class Complex:
-    def __init__(self):
-        pass
+    def __init__(self, real=0, imaginary=0):
+        if not isinstance(real, (int, float)):
+            raise TypeError("Вещественная часть должна быть числом.")
+        if not isinstance(imaginary, (int, float)):
+            raise TypeError("Мнимая часть должна быть числом.")
+        self.__real = real
+        self.__imaginary = imaginary
+
+    def __str__(self):
+        con = ''
+        if self.__imaginary > 0:
+            con = '+'
+        return f"{self.real} {con} {self.__imaginary}i"
 
     def __add__(self, other):
-        pass
+        real = self.__real + other.real
+        imaginary = self.__imaginary + other.imaginary
+        return Complex(real, imaginary)
 
     def __mul__(self, other):
-        pass
+        a, b = self.__real, self.__imaginary
+        c, d = other.real, other.imaginary
+        real = a * c - b * d
+        imaginary = b * c + a * d
+        return Complex(real, imaginary)
+
+    @property
+    def real(self):
+        return self.__real
+
+    @property
+    def imaginary(self):
+        return self.__imaginary
+
+
+z1 = Complex(-3.6, 8)
+z2 = Complex(15, -5)
+
+print(z1)
+print(z2)
+print(z1 + z2)
+print(z1 * z2)
 
